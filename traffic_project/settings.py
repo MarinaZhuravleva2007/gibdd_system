c726b61bc6fa700224aa3c12168fc29f5e4b32ca
MarinaZhuravleva2007 <marinazhuravleva07@inbox.ru>
2026-06-28 23:36:38 +0400

    Update settings.py

--- traffic_project/settings.py
+++ traffic_project/settings.py
@@ -69,7 +69,15 @@ TEMPLATES = [
     },
 ]
 # Mail.ru
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = 'marina.zhuravleva.07@inbox.ru'
+EMAIL_HOST_PASSWORD = '7GXMy7xL9gYdQeka4jXh'
+DEFAULT_FROM_EMAIL = 'marina.zhuravleva.07@inbox.ru'
+WSGI_APPLICATION = 'traffic_project.wsgi.application'
 
 # логирование для отладки
 LOGGING = {
